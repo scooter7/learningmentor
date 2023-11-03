55f759102a1dc3dd8d30eab44ea9e4a39b199ec8
scooter7 <scooter.vineburgh@gmail.com>
2023-11-02 20:00:49 -0500

    Update learningmentor.py

--- learningmentor.py
+++ learningmentor.py
@@ -1,68 +1,36 @@
 import openai
 import streamlit as st
 
-# Use Streamlit's secrets object to load the API key
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Function to send prompts to the OpenAI API
-def ask_openai(prompt_text):
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+def ask_openai(messages):
     response = openai.ChatCompletion.create(
         model="gpt-4",
-        messages=[
-            {"role": "system", "content": "You are a Leadership Coach."},
-            {"role": "user", "content": prompt_text}
-        ]
+        messages=messages
     )
     return response.choices[0].message['content'].strip()
 
-# Initialize the Streamlit app
 st.title('Leadership Mentor App')
 
-# Using a form to get user input
 with st.form(key='leadership_form'):
     user_input = st.text_area("Enter your leadership-related query or request:")
     submit_button = st.form_submit_button(label='Submit')
 
 if submit_button:
-    if user_input.lower().strip() == "create leadership growth plan":
-        # If the user wants to create a growth plan, ask a series of questions
-        st.header("Leadership Growth Plan")
-        goals = st.text_input("What are your main leadership goals?")
-        challenges = st.text_input("What challenges are you currently facing?")
-        successes = st.text_input("What have been your recent successes?")
-        if st.button('Generate Growth Plan'):
-            prompt_text = f"|Leadership Mentor:|\nCREATE LEADERSHIP GROWTH PLAN\nGoals: {goals}\nChallenges: {challenges}\nSuccesses: {successes}"
-            advice = ask_openai(prompt_text)
-            st.write(advice)
-
-    elif user_input.lower().strip() == "mentorship":
-        # If the user is seeking mentorship
-        st.header("Leadership Mentorship")
-        issue = st.text_input("What specific issue would you like to discuss?")
-        if st.button('Get Mentorship Advice'):
-            prompt_text = f"|Leadership Mentor:|\nMENTORSHIP\nIssue: {issue}"
-            advice = ask_openai(prompt_text)
-            st.write(advice)
-
-    elif user_input.lower().strip() == "coaching conversation examples":
-        # If the user wants examples of coaching conversations
-        st.header("Coaching Conversation Examples")
-        context = st.text_input("What is the context of the conversation you're looking to have?")
-        if st.button('Show Examples'):
-            prompt_text = f"|Leadership Mentor:|\nCOACHING CONVERSATION EXAMPLES\nContext: {context}"
-            advice = ask_openai(prompt_text)
-            st.write(advice)
-
-    elif user_input.lower().strip() == "leadership resources":
-        # If the user is looking for resources
-        st.header("Leadership Resources")
-        topic = st.text_input("What topic do you need resources for?")
-        if st.button('Provide Resources'):
-            prompt_text = f"|Leadership Mentor:|\nLEADERSHIP RESOURCES\nTopic: {topic}"
-            advice = ask_openai(prompt_text)
-            st.write(advice)
-    else:
-        # For any other general leadership advice
-        prompt_text = f"|Leadership Mentor:|\n{user_input}"
-        advice = ask_openai(prompt_text)
-        st.write(advice)
+    if user_input.lower().strip() in ["create leadership growth plan", "mentorship", "coaching conversation examples", "leadership resources"]:
+        st.session_state['messages'].append({"role": "system", "content": "You are a Leadership Coach."})
+        
+    user_message = {"role": "user", "content": user_input}
+    st.session_state['messages'].append(user_message)
+    
+    advice = ask_openai(st.session_state['messages'])
+    st.session_state['messages'].append({"role": "assistant", "content": advice})
+    
+    for message in st.session_state['messages']:
+        if message['role'] == 'user':
+            st.text_area("You", value=message['content'], disabled=True, key=message['content'])
+        elif message['role'] == 'assistant':
+            st.text_area("Leadership Mentor", value=message['content'], disabled=True, key=message['content'])
